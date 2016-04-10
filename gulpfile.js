dc4374c22dcee662afcbb6aada0e923712f6794b
Dan Allen <dan.j.allen@gmail.com>
2016-04-10 03:05:42 -0600

    use github-pages Gradle plugin to publish to gh-pages
    - add github-pages plugin
    - configure github-pages plugin
    - remove deploy task from Gulp
    - drop unused node packages
    - update node packages
    - remove unused Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,9 @@ var pkg = require('./package.json'),
   buffer = require('vinyl-buffer'),
   closureCompiler = require('google-closure-compiler').gulp(),
   csso = require('gulp-csso'),
-  del = require('del'),
-  ghpages = require('gh-pages'),
   gulp = require('gulp'),
   gutil = require('gulp-util'),
   jade = require('gulp-jade'),
-  path = require('path'),
   plumber = require('gulp-plumber'), // plumber prevents pipe breaking caused by errors thrown by plugins
   rename = require('gulp-rename'),
   source = require('vinyl-source-stream'),
@@ -23,22 +20,8 @@ var pkg = require('./package.json'),
     language_in: 'ES5_STRICT',
     language_out: 'ES5'
   },
-  //tidyOpts = {
-  //  'anchor-as-name': 'false',
-  //  'coerce-endtags': 'false',
-  //  'drop-empty-elements': 'false',
-  //  'fix-uri': 'false',
-  //  'indent': 'false',
-  //  'literal-attributes': 'true',
-  //  'newline': 'LF',
-  //  'preserve-entities': 'true',
-  //  'quiet': 'true',
-  //  'show-warnings': 'false',
-  //  'tidy-mark': 'false',
-  //  'wrap': '0'
-  //},
-  outputDir = 'build/bespoke',
-  isDist = process.argv.indexOf('deploy') >= 0;
+  isDist = gutil.env.dist,
+  outputDir = 'build/bespoke';
 
 gulp.task('js', function() {
   // see https://wehavefaces.net/gulp-browserify-the-gulp-y-way-bb359b3f9623
@@ -60,29 +43,6 @@ gulp.task('html_jade', function() {
     .pipe(gulp.dest(outputDir));
 });
 
-//gulp.task('asciidoc-html', function() {
-//  return gulp.src('src/content/index.adoc')
-//    .pipe(isDist ? through() : plumber())
-//    // NOTE using stdin here would cause loss of context
-//    //.pipe(exec('bundle exec asciidoctor-bespoke -o - src/content/index.adoc', { pipeStdout: true }))
-//    .pipe(exec('bundle exec asciidoctor-bespoke -T src/templates -o - src/content/index.adoc', { pipeStdout: true }))
-//    .pipe(exec.reporter({ stdout: false }))
-//    .pipe(through(function(file) {
-//      var html = tidy(file.contents.toString(), tidyOpts) // NOTE based on tidy 4.9.26
-//        // strip extra newlines inside <pre> tags (fixed in tidy 5.1.2)
-//        .replace(new RegExp('<pre([^>]*)>\\n([\\s\\S]*?)\\n</pre>', 'g'), '<pre$1>$2</pre>\n')
-//        // strip extra newline after <script> start tag for empty and single-line content
-//        .replace(new RegExp('>\\n(?:(.+)\\n)?</script>', 'g'), '>$1</script>')
-//        // add newline before <script> tags
-//        .replace(new RegExp('><script([^>]*)>', 'g'), '>\n<script$1>');
-//      file.contents = new Buffer(html);
-//      this.push(file);
-//    }))
-//    .pipe(rename('index.html'))
-//    .pipe(chmod(644))
-//    .pipe(gulp.dest(outputDir));
-//});
-
 gulp.task('css', function() {
   return gulp.src('src/app/styles/main.styl')
     .pipe(isDist ? through() : plumber())
@@ -93,21 +53,4 @@ gulp.task('css', function() {
     .pipe(gulp.dest(outputDir + '/build'));
 });
 
-gulp.task('clean:html_jade', function() {
-  return del(outputDir + '/index-jade.html');
-});
-
-gulp.task('clean:js', function() {
-  return del(outputDir + '/build/build.js');
-});
-
-gulp.task('clean:css', function() {
-  return del(outputDir + '/build/build.css');
-});
-
-gulp.task('deploy', function(done) {
-  ghpages.publish(path.join(__dirname, outputDir), { logger: gutil.log }, done);
-});
-
-gulp.task('clean', ['clean:js', 'clean:html_jade', 'clean:css']);
 gulp.task('build', ['js', 'html_jade', 'css']);
